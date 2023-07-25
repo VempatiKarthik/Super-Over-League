506cf974ea1b875770a8ce537df5912f5e33ccb1
VempatiKarthik <vempatikarthik79@gmail.com>
2023-07-25 16:31:04 +0530

    first test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Welcome from './components/Newapp';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Welcome />
+    </div>
+  )
+}
+
+export default App;
