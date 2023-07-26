64c27516a30211c21d7e729010ad220f91461784
VempatiKarthik <vempatikarthik79@gmail.com>
2023-07-26 17:33:42 +0530

    Super Over League

---
+++ src/MyComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return <div>
+    <h1 className="heading">Super Over League</h1>
+    <div className="imgContainer">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>;
+  
+};
+
+export default MyComponent;
---
+++ src/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // Add this line to set the mode to development
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
